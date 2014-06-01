bb02cfb952ea264db7ec8a778c6ff91479cb8aa6
Peter <peterhsaxton@gmail.com>
2014-06-01 14:53:17 +0100

    build

--- dist/SoVeryGroovy.js
+++ dist/SoVeryGroovy.js
@@ -93,3 +93,18 @@ var SVGroovy = {};
     matrixTransform: matrixTransform
   })(create);
 }(SVGroovy));
+(function(parent){
+  "use strict";
+
+  var darkSVG = document.createElementNS("http://www.w3.org/2000/svg", 'svg');
+  function create(){
+    return darkSVG.createSVGMatrix();
+  }
+  parent.Matrix = create;
+  parent.Matrix.scaling = function(scalar){
+    return create().scale(scalar);
+  };
+  parent.Matrix.translating = function(x, y){
+    return create().translate(x, y);
+  };
+}(SVGroovy));
