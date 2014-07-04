36d86ca18514bc1c70507b4719f881da6b37bfaa
Peter <peterhsaxton@gmail.com>
2014-07-04 15:54:41 +0100

    build

--- dist/SoVeryGroovy.js
+++ dist/SoVeryGroovy.js
@@ -3,7 +3,6 @@ var SVGroovy = {};
   "use strict";
 
   var darkSVG = document.createElementNS("http://www.w3.org/2000/svg", 'svg');
-  var origin = darkSVG.createSVGPoint();
 
   function create(x, y){
     if (isObj(x)) {
@@ -101,10 +100,10 @@ var SVGroovy = {};
     return darkSVG.createSVGMatrix();
   }
   parent.Matrix = create;
-  parent.Matrix.scaling = function(scalar){
+  parent.Matrix.toScale = function(scalar){
     return create().scale(scalar);
   };
-  parent.Matrix.translating = function(x, y){
-    return create().translate(x, y);
+  parent.Matrix.toTranslate = function(point){
+    return create().translate(point.x, point.y);
   };
 }(SVGroovy));
